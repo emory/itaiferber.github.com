9d70895f5f4b0bc3f825d6a8924f52fd6735241d
Itai Ferber <itaiferber@gmail.com>
2012-03-27 11:12:44 -0400

    Initialized project.
    -- Initialized Compass and configured.
    -- Created default page layout.

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Compass Directories
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# Output Style
+output_style = :compressed
